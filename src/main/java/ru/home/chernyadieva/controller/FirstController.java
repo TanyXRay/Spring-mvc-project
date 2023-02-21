723550beae0d85c4aa9cdb1dac870eb949a8f6ae
Tatyana Chernyadieva <tany.taray@gmail.com>
2023-02-21 11:52:35 +0400

    Spring-mvc-app
    реализован HTTP get- запрос с параметрами

--- src/main/java/ru/home/chernyadieva/controller/FirstController.java
+++ src/main/java/ru/home/chernyadieva/controller/FirstController.java
@@ -1,17 +1,35 @@
 package ru.home.chernyadieva.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
+    /**
+     * 2 способа передать нужные параметры в URL get-запрос
+     * @param name
+     * @param surname
+     * @return
+     */
     @GetMapping("/hello")
-    private String helloPage() {
+    private String helloPage(@RequestParam (value = "name", required = false) String name,
+                             @RequestParam(value = "surname", required = false) String surname) {
+        System.out.println("Hello, " + name + " " + surname);
         return "first/hello";
     }
+
+    //   @GetMapping("/hello")
+    //   private String helloPage(HttpServletRequest httpServletRequest) {
+    //       String name = httpServletRequest.getParameter("name");
+    //       String surname = httpServletRequest.getParameter("surname");
+    //       System.out.println("Hello, " +name + " " + surname);
+    //       return "first/hello";
+    //   }
 
     @GetMapping("/goodbye")
     private String goodbyePage() {
